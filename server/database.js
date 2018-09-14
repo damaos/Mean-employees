3c5850c78fda456b33972c045ed57bbce73248bb
Danilo Martinez <danilo_98osorno@hotmail.com>
2018-09-14 00:07:55 -0500

    Finish of API

--- server/database.js
+++ server/database.js
@@ -6,4 +6,5 @@ const URI = 'mongodb://localhost/mean-crud';
 mongoose.connect(URI)
     .then(db => console.log('Db is conect in ' + URI))
         .catch(err =>  console.error(err()));
+        
 module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const { mongoose } = require('./database');
 
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
 
 // routes 
 app.use('/api/employees/',require('./routes/employee.routes'));   
